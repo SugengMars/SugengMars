2c4e10dbc6f2d97b6b4d357e44652d9e1ffd5864
SugengMars <bamsum16@students.amiko.ac.id>
2022-04-08 13:53:16 +0700

    first commit

---
+++ produk.js
@@ -0,0 +1,39 @@
+let produk ={
+    "Shop" : [
+    {
+        "image": "img/p1.jpg",
+        "nama": "Mechanical keren",
+        "harga": "$46"
+    },
+    {
+        "image": "img/p2.jpg",
+        "nama": "Mechanical White",
+        "harga": "$46"
+    },
+    {
+        "image": "img/p3.jpg",
+        "nama": "Mechanical Pink",
+        "harga": "$46"
+    },
+    {
+        "image": "img/p4.jpg",
+        "nama": "Mechanical Black",
+        "harga": "$46"
+    },
+    {
+        "image": "img/p5.jpg",
+        "nama": "Mechanical Clean",
+        "harga": "$46"
+    },
+    {
+        "image": "img/p6.jpg",
+        "nama": "Mechanical Pink 0.2",
+        "harga": "$46"
+    }
+]
+}
+
+// function chatToAdmin(nama, harga){
+//     const noHp = "+6281807448231";
+//     window.open(`https://web.whatsapp.com/send?phone=${noHp}?&text=nama-produk:%20${nama}%0ADeskripsi:%20${desc}%0AHarga:%20${harga}`);
+// }
